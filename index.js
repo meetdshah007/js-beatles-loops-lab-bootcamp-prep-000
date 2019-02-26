ce93406f13cd2180fb259e8f56aa0c8a49d4f76d
Meet Shah <meetdshah007@gmail.com>
2019-02-26 11:00:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(magician, instruments) {
+  const arr = [];
+  for(let counter=0; counter< magician.length; counter++){
+    arr.push(`John Lennon plays guitar.`)
+  }
+  return arr;
+}
+
+function johnLennonFacs(){
+  
+}
