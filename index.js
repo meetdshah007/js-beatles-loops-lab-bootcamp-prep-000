d82fd55274e3a6cdc2baf09a3426648a8215d26a
Meet Shah <meetdshah007@gmail.com>
2019-02-26 11:06:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(magician, instruments) {
   const arr = [];
   for(let i=0; i< magician.length; i++){
-    arr.push(`${magician[i]} plays ${instruments[i]}.`)
+    arr.push(`${magician[i]} plays ${instruments[i]}`)
   }
   return arr;
 }
